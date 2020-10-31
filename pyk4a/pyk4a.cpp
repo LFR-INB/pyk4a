1744578c34e2a8ea2f8a8679e09239f6c2104678
crux24Home <crux2448@gmail.com>
2020-10-31 18:06:01 +0100

    added both arguments in timestamp func

--- pyk4a/pyk4a.cpp
+++ pyk4a/pyk4a.cpp
@@ -729,7 +729,7 @@ extern "C" {
 
         thread_state = _gil_release(thread_safe);
         *image = k4a_capture_get_color_image(*capture_handle);
-        image_timestamp_usec=k4a_image_get_device_timestamp_usec(*image);
+        image_timestamp_usec = k4a_image_get_device_timestamp_usec(*image);
         _gil_restore(thread_state);
 
         PyArrayObject* np_image;
@@ -738,7 +738,7 @@ extern "C" {
         }
 
         if (K4A_RESULT_SUCCEEDED == res) {
-            return Py_BuildValue("k", image_timestamp_usec);
+            return Py_BuildValue("kO", image_timestamp_usec,PyArray_Return(np_image));
             //PyArray_Return(np_image);
         }
         else {
