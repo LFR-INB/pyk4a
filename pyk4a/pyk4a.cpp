a256880912048c760a8f6af993682655bf2ef60c
crux24Home <crux2448@gmail.com>
2020-10-31 19:35:25 +0100

    tupel test

--- pyk4a/pyk4a.cpp
+++ pyk4a/pyk4a.cpp
@@ -742,7 +742,7 @@ extern "C" {
             //return Py_BuildValue("kO", image_timestamp_usec,PyArray_Return(np_image));
             PyObject *tupleresult = PyTuple_New(2);
             PyTuple_SetItem(tupleresult, 0, PyArray_Return(np_image));
-            PyTuple_SetItem(tupleresult, 1, image_timestamp_usec);
+            PyTuple_SetItem(tupleresult, 1, PyArray_Return(np_image));
             return tupleresult;
 //            PyObject *MyResult =  Py_BuildValue("Ok", np_image, image_timestamp_usec);
 //            Py_DECREF(np_image);
